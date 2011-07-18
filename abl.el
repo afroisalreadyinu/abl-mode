03906934ec726c1739036c9551fc5b198aab6284
Afrois Alreadyinu <afroisalreadyinu@gmail.com>
2011-07-18 16:13:26 +0200

    initial commit

---
+++ abl.el
@@ -0,0 +1,508 @@
+
+;; The aim of this mode is to make editing Python code in a
+;; version-controlled project easier, and make the repetitive tasks
+;; such as running tests and bits of code easier. It takes the
+;; organization of code in a certain way as granted.
+
+;; <<--------- The necessary minor-mode stuff  ---------->>
+
+(defvar abl-mode nil
+  "Mode variable for abl-mode")
+(make-variable-buffer-local 'abl-mode)
+
+(defun abl-mode (&optional arg)
+  "abl minor mode"
+  (interactive "P")
+  (setq abl-mode (if (null arg) (not abl-mode)
+		   (> (prefix-numeric-value arg) 0)))
+  (if abl-mode
+      (let ((project-base (find-base-dir)))
+	(if (not project-base)
+	    (setq abl-mode nil)
+	  (setq abl-branch-base project-base)
+	  (setq abl-branch (branch-name abl-branch-base))
+	  (setq project-name (get-project-name abl-branch-base))
+	  (setq vem-name (get-vem-name))
+	  ))))
+
+(defun abl-mode-hook ()
+  (abl-mode))
+
+(if (not (assq 'abl-mode minor-mode-alist))
+    (setq minor-mode-alist
+	  (cons '(abl-mode " abl-mode")
+		minor-mode-alist)))
+
+(defvar abl-mode-keymap
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c r") 'run-current-branch)
+    (define-key map (kbd "C-c t") 'run-test-at-point)
+    (define-key map (kbd "C-c u") 'rerun-last-test)
+    (define-key map (kbd "C-c e") 'create-etags-file)
+    (define-key map (kbd "C-c o") 'open-python-path-at-point)
+    (define-key map (kbd "C-c a") 'revert-all-buffers)
+    (define-key map (kbd "C-c w") 'display-branch)
+    (define-key map (kbd "C-c s") 'start-vem-python)
+    map)
+  "The keymap for abl-mode")
+
+(or (assoc 'abl-mode minor-mode-map-alist)
+    (setq minor-mode-map-alist
+          (cons (cons 'abl-mode abl-mode-keymap)
+                minor-mode-map-alist)))
+
+;; <<------------  Variables  -------------->>
+
+(defcustom vem-activate-command "vem_activate %s"
+  "The command for activating a virtual environment")
+
+(defcustom vem-create-command "vem create %s"
+  "The command for activating a virtual environment")
+
+(defcustom test-command "nosetests -s %s"
+  "The command for running tests")
+
+(defcustom branch-shell-prefix "ABL-SHELL:"
+  "Prefix for the shell buffers opened")
+
+(defcustom vems-base-dir "~/.virtualenvs"
+  "base directory for virtual environments")
+
+(defcustom start-server-command "scripts/run.sh"
+  "command executed for starting the local server of a web
+  application.")
+
+(defcustom complete-testrun-command "echo '%s' && %s"
+  "This is the actual command which is executed for tests; the
+  test command is formatted into it twice in order to get display
+  of what actually happens in the shell buffer")
+
+;; <<----------------  Here ends the customization -------------->>
+
+(defvar abl-branch-base ""
+  "Base directory of the current branch")
+(make-variable-buffer-local 'abl-branch-base)
+
+(defvar vem-name ""
+  "Name of the virtual env")
+(make-variable-buffer-local 'vem-name)
+
+(defvar etags-command-base "find %s -name '*.py' -print | etags - -o %s/TAGS"
+  "command run to create a tags file for emacs")
+
+(defvar abl-branch "master"
+  "The branch you are working on.When abl-mode is started, it is
+  set to the name of the directory in which you are for svn, the
+  git branch if you're on git.")
+(make-variable-buffer-local 'abl-branch)
+
+(defvar project-name "web"
+  "The name of the project. ")
+(make-variable-buffer-local 'project-name)
+
+(defvar last-test-run nil
+  "Last test run and which branch it was")
+
+(defvar existing-shells '())
+
+(defvar replacement-vems '())
+
+;; <<------------- Helpers  ------------->>
+
+(defun starts-with (str1 str2)
+  (and (> (length str1) 0)
+       (string= str2
+		(substring str1 0 (length str2)))))
+
+(defun ends-with (str1 str2)
+  (let ((str1-length (length str1)))
+    (and (> str1-length 0)
+	 (string= (substring str1 (- str1-length (length str2)) str1-length) str2))))
+
+(defun remove-last (lst)
+  (if (not (cdr lst))
+      '()
+    (cons (car lst) (remove-last (cdr lst)))))
+
+(defun index-of (substr str1)
+  (cond ((< (length str1) (length substr)) nil)
+	((string= substr (substring str1 0 (length substr))) 0)
+	(t (let ((rest-return (index-of substr (substring str1 1 (length str1)))))
+	     (if (null rest-return) nil
+	       (+ rest-return 1))))))
+
+(defun concat-paths (base &rest paths)
+  "join a list of path components into a path"
+  (if (equal paths '())
+      base
+    (apply 'concat-paths
+	   (concat (file-name-as-directory base) (car paths))
+	   (cdr paths))))
+
+(defun remove-last-slash (path)
+  (if (ends-with path "/")
+      (substring path 0 (- (length path) 1))
+    path))
+
+(defun higher-dir (path)
+  "Return one higher directory of a given path"
+  (assert (starts-with path "/"))
+  (if (string-equal "/" path)
+      nil
+    (let* ((true-path (remove-last-slash path))
+	   (components (split-string true-path "/" )))
+      (apply 'concat-paths
+	     (concat "/" (car components))
+	     (remove-last (cdr components))))))
+
+(defun last-path-comp (path)
+  "Get the last path components, whether it's a file name or directory"
+  (and (< 0 (length path))
+       (car (last (split-string (remove-last-slash path) "/")))))
+
+(defun find-base-dir (&optional dir-path)
+  (let* ((path (or dir-path (buffer-file-name))))
+    (if (and (file-exists-p (concat-paths path "setup.py"))
+	     (not (file-exists-p (concat-paths path "__init__.py"))))
+	path
+      (let ((higher (higher-dir path)))
+	(if (not higher)
+	    nil
+	  (find-base-dir higher))))))
+
+(defun git-or-svn (base-dir)
+  (cond ((file-exists-p (concat-paths base-dir ".git")) "git")
+	((file-exists-p (concat-paths base-dir ".svn")) "svn")
+	(t nil)))
+
+(defun get-git-branch-name (base-dir)
+  (let* ((command (concat "cd " base-dir " && git branch"))
+	(git-output (shell-command-to-string command)))
+    (if (string-equal git-output "")
+	(error "Looks like your git repository is empty (the output of git branch was empty). Please commit your changes and activate abl-mode again..")
+      (string-match "\\* \\(.*\\)" git-output)
+      (substring git-output (match-beginning 1) (match-end 1)))))
+
+
+(defun branch-name (path)
+  (if (string= path "/")
+      nil
+    (let ((vcs (git-or-svn path)))
+      (cond ((or (not vcs) (string-equal vcs "svn"))
+	     (last-path-comp path))
+	    ((string-equal vcs "git")
+	     (get-git-branch-name path))
+	    (t nil)))))
+
+
+(defun get-project-name (path)
+  "Returns the name of the project; higher directory for no vcs or svn,
+   directory name for git."
+  (if (string= path "/")
+      nil
+    (let ((vcs (git-or-svn path)))
+      (cond ((or (not vcs) (string-equal vcs "svn"))
+	     (last-path-comp (higher-dir path)))
+	    ((string-equal vcs "git")
+	     (last-path-comp path))
+	    (t nil)))))
+
+(defun get-vem-name (&optional branch project)
+  (let ((branch-name (or branch abl-branch))
+	(prjct-name (or project project-name)))
+    (or
+     (cdr (assoc branch-name replacement-vems))
+     (concat prjct-name "_" branch-name))))
+
+;;<< ---------------  Shell stuff  ----------------->>
+
+(defun shell-name-for-branch (project-name branch-name)
+  (concat branch-shell-prefix project-name "_" branch-name))
+
+
+(defun create-or-switch-to-branch-shell ()
+  (let ((shell-name (shell-name-for-branch project-name abl-branch)))
+    (unless (member shell-name existing-shells)
+      (save-excursion (shell shell-name))
+      (run-shell-command (concat "cd " abl-branch-base)
+			 shell-name)
+      (vem-exists-create vem-name shell-name)
+      (run-shell-command (format vem-activate-command vem-name)
+			 shell-name)
+      (setf existing-shells (append existing-shells '(shell-name))))
+    (if (> (length (get-buffer-window-list shell-name nil t)) 1)
+	(delete-window))
+    shell-name))
+
+
+(defun vem-name-or-create (name)
+  (let ((vem-path (expand-file-name name vems-base-dir)))
+    (if (file-exists-p vem-path)
+	(cons name nil)
+      (let*
+	  ((command-string (format "No vem %s; y to create it, or name of existing to use instead: "
+				   name))
+	   (vem-or-y (read-from-minibuffer command-string))
+	   (create-new (or (string-equal vem-or-y "y") (string-equal vem-or-y "Y"))))
+	(if create-new
+	    (cons name create-new)
+	  (vem-name-or-create vem-or-y))))))
+
+
+(defun vem-exists-create (name shell-name)
+  (let* ((new-or-name (vem-name-or-create name))
+	 (create-vem (cdr new-or-name))
+	 (new-vem-name (car new-or-name)))
+    (if create-vem
+	(progn
+	  (run-shell-command (format vem-create-command new-vem-name) shell-name)
+	  (run-shell-command (format vem-activate-command new-vem-name) shell-name)
+	  (run-shell-command "python setup.py develop" shell-name)))
+    (if (not (string-equal new-vem-name name))
+	(progn (setq replacement-vems (cons '(name . new-vem-name) replacement-vems))
+	       (setq vem-name new-vem-name)))))
+
+
+(defun run-shell-command (command buffer-name)
+  (process-send-string buffer-name
+		       (concat command "\n")))
+
+
+(defun run-shell-command-for-branch (command branch-name)
+  (save-excursion
+    (let ((shell-name (shell-name-for-branch project-name abl-branch)))
+      (if (not (member shell-name existing-shells))
+	  (create-or-switch-to-branch-shell))
+      (run-shell-command command shell-name))))
+
+
+(defun run-command-for-abl-branch (command)
+  (run-shell-command-for-branch command abl-branch))
+
+
+;; <<------------  Running the server and tests  -------->>
+
+(defun run-current-branch ()
+  (interactive)
+  (run-command-for-abl-branch start-server-command)
+  (message (format "Started local server for branch %s" abl-branch)))
+
+
+(defun determine-test-function-name ()
+  (save-excursion
+    (end-of-line)
+    (if (not (re-search-backward "^ *def test_*" nil t))
+	(error "Looks like you are not even in a function definiton! Bad girl!"))
+    (let* ((start (re-search-forward "^ *def *"))
+	   (end (re-search-forward "test_[^\(]*" (line-end-position) t)))
+      (if (not end)
+	  (error "Looks like you are not inside a test function. Go to a test function! Now!")
+	(buffer-substring-no-properties start (point))))))
+
+
+(defun determine-test-class-name ()
+  (save-excursion
+    (if (not (re-search-backward "^class *" nil t))
+	(error "Looks like there is a problem with your python code (functions is indented
+but not in a class). Sorry, can't do anything")
+    (let* ((start (re-search-forward "^class *"))
+	   (end (re-search-forward "[^\(:]*" (line-end-position) t)))
+      (if (not end)
+	  (error "Looks like there is a problem with you python code (keyword class not
+followed by a proper class name).")
+	(buffer-substring-no-properties start (point)))))))
+
+
+;;this function assumes that you are already in a test function (see
+;;the function above)
+(defun test-in-class ()
+  (save-excursion
+    (end-of-line)
+    (let* ((start (re-search-backward "^ *def *"))
+	   (end (re-search-forward "[^ ]")))
+      (> (- end start 1) 0))))
+
+
+(defun get-test-file-path ()
+  (let ((buffer-name (buffer-file-name)))
+    (if (not (ends-with buffer-name ".py"))
+	(error "You do not appear to be in a python file. Now open a python file!"))
+    (let ((relative-path (substring
+			  buffer-file-name
+			  (+ (length abl-branch-base) 1)
+			  (- (length buffer-name) 3))))
+      (replace-regexp-in-string "/" "." relative-path))))
+
+
+(defun get-test-function-path (file-path)
+  (let ((function-name (determine-test-function-name)))
+    (if (not (test-in-class))
+	(concat file-path ":" function-name)
+      (let ((class-name (determine-test-class-name)))
+	(concat file-path ":" class-name "." function-name)))))
+
+
+(defun run-test (test-path &optional branch-name)
+  (let* ((test-command-torun (format test-command test-path))
+	 (shell-command (format complete-testrun-command test-command-torun test-command-torun))
+	 (real-branch-name (or branch-name abl-branch)))
+    (message (concat "Running test(s): " test-path))
+    (run-shell-command-for-branch shell-command real-branch-name)
+    (setq last-test-run (cons test-path abl-branch))))
+
+
+;This returns the python destination on point, depending on
+;whether it is a test function, class, or whole file
+(defun get-test-entity ()
+  (let ((file-path (get-test-file-path)))
+    (if (= (line-number-at-pos) 1)
+	file-path
+      (let* ((test-func-pos
+	      (save-excursion
+		(re-search-backward "^ *def test*" nil t)))
+	     (test-class-pos
+	      (save-excursion
+		(re-search-backward "^class *" nil t))))
+	(cond
+	 ((not (or test-func-pos test-class-pos))
+	  (error "You are neither in a test class nor a test function."))
+	 ((and test-func-pos
+	       (and test-class-pos (< test-class-pos test-func-pos)))
+	  (get-test-function-path file-path))
+	 (test-class-pos (concat file-path ":" (determine-test-class-name))))))))
+
+(defun run-test-at-point ()
+  (interactive)
+  (let* ((test-path (get-test-entity)))
+    (run-test test-path)))
+
+(defun rerun-last-test ()
+  (interactive)
+  (if (not last-test-run)
+      (message "You haven't run any tests yet.")
+    (run-test (car last-test-run) (cdr last-test-run))))
+
+;the command: "find . -name *.py -print | etags -"
+(defun create-etags-file ()
+  (interactive)
+  (let ((tag-file-path (concat-paths abl-branch-base "TAGS")))
+    (if (or (not (file-exists-p tag-file-path))
+	    (if (y-or-n-p "Tags file alread exists, recreate?")
+		(progn
+		  (delete-file tag-file-path)
+		  t)))
+	(let ((etags-command (format etags-command-base
+				     abl-branch-base
+				     abl-branch-base)))
+	  (shell-command-to-string etags-command)))))
+
+
+(defun string-in-buffer (string)
+  (save-excursion
+    (goto-char (point-min))
+    (if (search-forward string nil t)
+	t
+      nil)))
+
+
+(defun join-string (string-list joiner)
+  (cond ((not (cdr string-list)) (car string-list))
+	 (t (concat
+	     (car string-list)
+	     joiner
+	     (join-string (cdr string-list) joiner)))))
+
+(defun starts-uppercase? (strng)
+  (let ((y (substring strng 0 1))) (string= y (upcase y))))
+
+
+(defun parse-python-path (python-path)
+  (let* ((colon-index (string-match ":" python-path))
+	 (file-part (if colon-index
+			(substring python-path 0 colon-index)
+		      python-path))
+	 (file-path (expand-file-name
+		     (concat (join-string (split-string file-part "\\.") "/") ".py")
+		     abl-branch-base))
+	 (internal-part (if colon-index
+			    (substring python-path (+ colon-index 1) (length python-path))
+			  nil))
+	 (internal-part-dot-index (if internal-part (string-match "\\." internal-part) nil)))
+    (let ((class-and-func-name
+	   (cond (internal-part-dot-index
+		  (cons (substring internal-part 0 internal-part-dot-index)
+			(substring internal-part (+ internal-part-dot-index 1)
+				   (length internal-part))))
+		 ((and internal-part (not internal-part-dot-index) (starts-uppercase? internal-part))
+		  (cons internal-part nil))
+		 (t (cons nil internal-part)))))
+      (list file-path (car class-and-func-name) (cdr class-and-func-name)))))
+
+
+(defun open-python-path-at-point ()
+  (interactive)
+  (save-excursion
+    (re-search-backward "[^a-zA-Z0-9:_\.]" nil t)
+    (forward-char)
+    (let ((start (point))
+	  (end (- (re-search-forward "[^a-zA-Z0-9:_\.]" nil t) 1)))
+      (let ((python-path-info (parse-python-path (buffer-substring-no-properties start end))))
+	(let ((file-path (car python-path-info))
+	      (class-name (cadr python-path-info))
+	      (func-name (caddr python-path-info)))
+	  (if (not (file-exists-p file-path))
+	      (error (concat "File coud not be found: " file-path)))
+	  (find-file file-path)
+	  (goto-char (point-min))
+	  (if class-name (search-forward (concat "class " class-name)))
+	  (if func-name (search-forward (concat "def " func-name))))))))
+
+
+(defun revert-or-skip (&optional buff)
+  "Reverts a buffer in abl-mode if it is not modified"
+  (let ((buffer (or buff (current-buffer))))
+    (when (and (local-variable-p 'abl-mode)
+	       (buffer-local-value 'abl-mode buffer))
+      (message (buffer-name buffer))
+      (if (buffer-modified-p buffer)
+	  (progn (message (concat "Buffer " (buffer-name buffer) " is modified"))
+		 (buffer-name buffer))
+      (progn
+	(set-buffer buffer)
+	(revert-buffer t t t)
+	(abl-mode t)
+	nil
+	)))))
+
+(defun revert-all-buffers()
+  "Refreshs all non-modified open buffers with the function above"
+  (interactive)
+  (let ((modified-files (delq nil (mapcar 'revert-or-skip (buffer-list)))))
+    (if modified-files
+	(message (concat "Following buffers have modifications: " (join-string modified-files " , ")))
+      (message "Refreshed open files"))))
+
+(defun display-branch()
+  "Displays the name of the branch on which the current buffer is"
+  (interactive)
+  (message (concat "Current branch: " abl-branch)))
+
+(defun start-vem-python ()
+  (interactive)
+  (ansi-term
+   (expand-file-name "python" (concat-paths vems-base-dir  vem-name "bin"))
+   (concat "Python " vem-name)))
+
+(provide 'abl)
+
+
+;; <<------------  TODOS -------------->>
+
+;; - create tags file and find files in project.
+;; - create etags file progressively by first finding all the code files in the dir
+;; - don't do git branch for every new buffer; make an assoc-list of bases (works now, as an improvement)
+
+
+;; Features:
+;; - add import (looks at other files, maybe in the order of last
+;;   visited, and finds suitable imports to insert
---
+++ test.el
@@ -0,0 +1,141 @@
+(require 'abl)
+(require 'ert)
+
+(defun write-to-file (file-path string)
+  (with-temp-buffer (insert string)
+		    (write-region (point-min) (point-max) file-path)))
+
+(defun setup-git-tests (&optional base)
+  ;create git repo with setup.py and a test file
+  (let* ((project-name "aproject")
+	 (base-dir (or base (make-temp-file "blah" 't)))
+	 (project-dir (concat-paths base-dir "aproject")))
+    (assert (index-of "Initialized empty Git repository"
+		      (shell-command-to-string
+		       (concat "git init " base-dir))))
+    (if (not (file-exists-p base-dir)) (make-directory base-dir))
+    (make-directory project-dir)
+    (write-to-file (concat-paths base-dir "setup.py") "blah")
+    (write-to-file (concat-paths project-dir "test.py") "blah")
+    base-dir))
+
+(defun commit-git (base-path)
+  (shell-command-to-string (format
+			    "cd %s && git add setup.py && git commit -am 'haha'"
+			    base-path)))
+
+(defun branch-git (base-path branch-name)
+  (shell-command-to-string (format
+			    "cd %s && git branch %s && git checkout %s"
+			    base-path branch-name branch-name)))
+
+(defun cleanup (path)
+  (shell-command-to-string
+   (concat "rm -rf " path)))
+
+(defun create-dummy-project ()
+  (let* ((base-dir (make-temp-file "yada" 't))
+	 (next-dir (concat-paths base-dir "etc"))
+	 (another-dir (concat-paths next-dir "blah")))
+    (make-directory next-dir)
+    (make-directory another-dir)
+    (write-to-file (concat-paths base-dir "setup.py") "blah")
+    another-dir))
+
+
+(ert-deftest test-abl-utils ()
+  (should (string-equal (concat-paths "/tmp/blah" "yada" "etc")
+			"/tmp/blah/yada/etc"))
+  (should (equal (remove-last '(1 2 3 4)) '(1 2 3)))
+  (should (equal (remove-last '(1)) '()))
+  (should (string-equal (higher-dir "/home/username/temp") "/home/username"))
+  (should (string-equal (higher-dir "/home/username/") "/home"))
+  (should (string-equal (higher-dir "/home") "/"))
+  (should (not(higher-dir "/")))
+  (should (string-equal (remove-last-slash "/hehe/haha") "/hehe/haha"))
+  (should (string-equal (remove-last-slash "/hehe/haha/") "/hehe/haha"))
+  (should (string-equal (remove-last-slash "") ""))
+  (should (string-equal (last-path-comp "/hehe/haha") "haha"))
+  (should (string-equal (last-path-comp "/hehe/haha/") "haha"))
+  (should (string-equal (last-path-comp "/hehe/haha.py") "haha.py"))
+  (should (not (last-path-comp "")))
+  )
+
+(ert-deftest test-path-funcs ()
+  (should (not (find-base-dir "/home")))
+  (let ((path (create-dummy-project)))
+    (should (string-equal (find-base-dir path)
+			  (higher-dir (higher-dir path))))
+    (should (string-equal (find-base-dir (higher-dir (higher-dir path)))
+			  (higher-dir (higher-dir path))))
+    (let* ((base-path (find-base-dir path))
+	   (git-path (concat-paths base-path ".git"))
+	   (svn-path (concat-paths base-path ".svn")))
+      (should (not (git-or-svn base-path)))
+      (make-directory git-path)
+      (should (string-equal (git-or-svn base-path) "git"))
+      (cleanup git-path)
+      (make-directory svn-path)
+      (should (string-equal (git-or-svn base-path) "svn"))
+      (cleanup base-path)
+    )))
+
+
+(ert-deftest test-project-name-etc ()
+  (should (string-equal (branch-name "/home") "home"))
+  (let* ((top-dir (make-temp-file "blah" 't))
+	 (top-dir-name (last-path-comp top-dir))
+	 (project-path (concat-paths top-dir "project")))
+    (setup-git-tests project-path)
+    (commit-git project-path)
+    (should (string-equal (branch-name project-path) "master"))
+    (should (string-equal (get-project-name project-path) "project"))
+
+    (should (string-equal (get-vem-name "master" "project")
+			  "project_master"))
+
+    (cleanup (concat-paths project-path ".git"))
+    (make-directory (concat-paths project-path ".svn"))
+    (should (string-equal (branch-name project-path) "project"))
+    (should (string-equal (get-project-name project-path) top-dir-name))
+    (cleanup top-dir)
+    ))
+
+
+(defun abl-values-for-path (path)
+  (let ((buffer (find-file path)))
+    (list
+     (buffer-local-value 'abl-mode buffer)
+     (buffer-local-value 'abl-branch buffer)
+     (buffer-local-value 'abl-branch-base buffer)
+     (buffer-local-value 'project-name buffer)
+     (buffer-local-value 'vem-name buffer))))
+
+
+(ert-deftest test-git-abl ()
+  (let* ((base-dir (setup-git-tests))
+	 (project-name (last-path-comp base-dir))
+	 (test-file-path (concat-paths base-dir "aproject" "test.py")))
+    (should-error (find-file test-file-path))
+    (commit-git base-dir)
+    (let ((abl-values (abl-values-for-path test-file-path)))
+      (should (car abl-values))
+      (should (string-equal "master" (nth 1 abl-values)))
+      (should (string-equal base-dir (nth 2 abl-values)))
+      (should (string-equal project-name (nth 3 abl-values)))
+    (cleanup base-dir)))
+
+
+(ert-deftest test-branched-git-abl ()
+  (let* ((base-dir (setup-git-tests))
+	 (project-name (last-path-comp base-dir))
+	 (test-file-path (concat-paths base-dir "aproject" "test.py")))
+    (should-error (find-file test-file-path))
+    (commit-git base-dir)
+    (branch-git base-dir "gitbranch")
+    (let ((abl-values (abl-values-for-path test-file-path)))
+      (should (car abl-values))
+      (should (string-equal "gitbranch" (nth 1 abl-values)))
+      (should (string-equal base-dir (nth 2 abl-values)))
+      (should (string-equal project-name (nth 3 abl-values)))
+    (cleanup base-dir))))
