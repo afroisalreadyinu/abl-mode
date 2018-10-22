25d5f0f269fa75e688a0b7d53fee70a93157cd37
Ulas Turkmen <ulas.turkmen@twylahelps.com>
2018-10-22 22:49:03 +0200

    Added command to create sandbox

--- test.el
+++ test.el
@@ -56,7 +56,6 @@
       (setq count (count-matches "t"))
       (kill-buffer))
     count))
-(character-count "/Users/ulas/temp/yada.txt" "t")
 
 (cl-defstruct (testenv
 	       (:constructor new-testenv
@@ -424,9 +423,45 @@ vem is created."
 	(ert `(member ,@tests))
       (ert 't))))
 
+(defvar init-file-content
+  (string-join
+   '("(custom-set-variables"
+     " '(package-user-dir \"%s\"))"
+     "(require 'package)"
+     "(add-to-list 'package-archives (cons \"melpa\" \"https://melpa.org/packages/\") t)"
+     "(package-initialize)"
+     "(when (not package-archive-contents)"
+     "  (package-refresh-contents))")
+   "\n"))
+
+(defvar install-file-content
+  (string-join
+   '("(require '%s)"
+     "(find-file (find-lisp-object-file-name '%s 'defvar))"
+     "(let ((package (package-buffer-info)))"
+     "  (package-download-transaction"
+     "   (package-compute-transaction nil (package-desc-reqs package))))")
+   "\n"))
+
 (defun create-package-sandbox ()
+  """Create a sandbox directory with necessary files, spit out command to install"""
   (interactive)
-  (let ((package (save-excursion (package-buffer-info))))
-    (message (format "%s" (package-desc-reqs package)))))
+  (let* ((package (save-excursion (package-buffer-info)))
+	 (sandbox-directory (format "~/temp/%s-sandbox" (package-desc-name package)))
+	 (init-file-path (abl-mode-concat-paths sandbox-directory "init.el"))
+	 (install-file-path (abl-mode-concat-paths sandbox-directory "install.el"))
+	 (packages-dir (abl-mode-concat-paths sandbox-directory "packages")))
+    ;; create base directory
+    (make-directory sandbox-directory)
+    ;; write init.el
+    (write-to-file init-file-path (format init-file-content packages-dir))
+    ;; write install file
+    (write-to-file install-file-path
+		   (format install-file-content
+			   (package-desc-name package)
+			   (package-desc-name package)))
+    (makedir packages-dir)))
+
+
 
 (run-elisp-tests)
